0eb9cc291f5bb72c3f754f98977650370282ea81
Danielalado <faithdigitalhub25gmail.com>
2025-11-13 12:50:13 +0000

    Worked on ConditionalFunctions

--- functions/intro.js
+++ functions/intro.js
@@ -74,7 +74,7 @@ function calculateRetangleArea (length, width) {
     console.log(multiply)
 }
 
-calculateRetangleArea (20, 44533)
+calculateRetangleArea (20, 44)
 
 
 function greet (name) {
@@ -83,3 +83,93 @@ function greet (name) {
 }
 
 greet ("Selorm");
+
+
+//Create a JavaScript object  with the properties name, engine, and color.
+//Then, write a function that takes the car object as a parameter and returns a string in this format:
+
+//"The name of my car is [name], it is [color] in color and it has a [engine] engine."
+ 
+//Answer
+
+let car = {
+    nAme: "BMW",
+    engine: 4.8,
+    color: "Blueblack",
+};
+
+function carName (car) {
+  return  console.log(`The name of my car is ${car.nAme}, it is ${car.color} in color and it has a ${car.engine}`)
+}
+
+carName (car)
+
+
+
+//conditional statements
+
+// if
+// if ...else
+// if ...else if
+
+// sythax if
+// if (condition) {
+    //code to execute if condition is true
+// }
+
+// sythax if ...if
+// if (condition) {
+    //code to execute if condition is true
+// } else {
+    // code to execute if condition is false
+// }
+
+
+// let x = 12;
+// let y = 3;
+
+// if (x > y){
+//     console.log(`The number ${x} is greater than the number ${y}`);
+// } else{
+//     console.log(`The number ${x} is rather less than the number ${y}`);
+// }
+
+
+//trial
+// let age = 30;
+
+// if (age < 18) {
+//     console.log (`The person is not eligible to vote`);
+// } else{
+//     console.log(`The person is eligible to vote`)
+// }
+
+
+
+// if (condition) {
+//    //code to run 
+// } else if (condition) {
+//    //code to run 
+// } else if (condition) {
+//     //code to run
+// } else {
+
+// }
+
+
+// trial 
+
+let temperature = 15;
+
+if (temperature > 25)
+    {
+    console.log(`It is extremely hot in Ghana`)
+} else if (temperature > 15) 
+    {
+    console.log(`It is a hot day`)
+} else if (temperature > 10) 
+    {
+    console.log(`It is not a hot day`)
+} else {
+    console.log(`It is cold day`)
+};
